d40bb31405f27663ed18e61188b0f421eca08b15
ImamSahroni <47499869+ImamSahroni@users.noreply.github.com>
2019-02-21 10:44:52 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
++++<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="Jin tomang anak peler";
+
+//user_gaid wajib isi
+$user_gaid= "bbc4a5a1-309e-4deb-b002-1777b28624a5";
+//user_token wajib di isi //
+$user_token="Abz9EmWqw6U7ouRP";
+
+//user_id wajib di isi //
+$user_id="Facebook_2087823104671550";
+
+// invite_code wajib di isi //
+$invite_code="F9YrIO1Clq";
+
+// Bearer wajib di isi //
+$Bearer="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18yMDg3ODIzMTA0NjcxNTUwIiwiZXhwIjoiMTU1MTY3MjI2NjEyOCJ9.wJnC8SIb8Gtm8rBwwPNPW5ySE1jF5uPa2HZ_SfeeXm4";
+
+
+
+?>
